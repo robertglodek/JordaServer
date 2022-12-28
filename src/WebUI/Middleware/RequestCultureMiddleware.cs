c19b6e711f1a9c1aa09b891a85d5d7a99553f50f
Robert Głodek <robert.glodek98@gmail.com>
2022-12-28 15:22:55 +0100

    Culture middleware changes

--- src/WebUI/Middleware/RequestCultureMiddleware.cs
+++ src/WebUI/Middleware/RequestCultureMiddleware.cs
@@ -7,27 +7,28 @@ public class RequestCultureMiddleware : IMiddleware
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var cultureQuery = context.Request.Query["culture"];
+
+        CultureInfo? culture = null;
         if (!string.IsNullOrWhiteSpace(cultureQuery))
         {
-            var culture = new CultureInfo(cultureQuery!);
-
-            CultureInfo.CurrentCulture = culture;
-            CultureInfo.CurrentUICulture = culture;
+            culture = new CultureInfo(cultureQuery!);
         }
         else if (context.Request.Headers.ContainsKey("Accept-Language"))
         {
             var cultureHeader = context.Request.Headers["Accept-Language"];
             if (cultureHeader.Any())
             {
-                var culture = new CultureInfo(cultureHeader.First()!.Split(',').First().Trim());
-
-                CultureInfo.CurrentCulture = culture;
-                CultureInfo.CurrentUICulture = culture;
+                culture = new CultureInfo(cultureHeader.First()!.Split(',').First().Trim());
             }
         }
+        if(culture == null)
+        {
+            culture = new CultureInfo("en-US");
+        }
+        
+        CultureInfo.CurrentCulture = culture;
+        CultureInfo.CurrentUICulture = culture;
 
         await next(context);
     }
-
-   
 }
